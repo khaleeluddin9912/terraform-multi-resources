9a490f4f1287e6f1e7985530fd3e7f26c2836c78
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-09-19 22:17:43 +0530

    Update variables.tf

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -1,1 +1,14 @@
+variable "subnet_map" {
+    type = map(object({
+        cidr_block = string
+        availability_zone = string
+    }))
+}
 
+variable "ec2_map" {
+    type = map(object({
+        ami           = string
+        instance_type = string
+    }))
+  
+}
